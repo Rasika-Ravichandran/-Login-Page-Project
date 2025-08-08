21e1fc806529b32d49df745bcbd8d3f2f80872ef
Rasika-Ravichandran <rasikaravi2022@gmail.com>
2025-08-08 06:19:13 -1200

    Add files via upload

---
+++ login page/us2.js
@@ -0,0 +1,11 @@
+
+function message(){
+var us=document.getElementById("username").value;
+var pd=document.getElementById("password").value;
+if(us=="Rasika-Ravichandran" && pd=="Rasika@2005"){
+    alert("sucessfully login....");
+    }
+else{
+    alert("invalid username and password");
+}
+}
